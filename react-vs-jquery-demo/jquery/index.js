3e249b587a559f820ef883b55c271d265d9c35e1
cobish <cobish110@gmail.com>
2016-07-19 19:28:10 +0800

    demo

---
+++ react-vs-jquery-demo/jquery/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    // 设置按钮不可用
+    $('.btn').prop('disabled', true);
+
+    // 监听文本框输入，如果有值，则设置按钮可用
+    $('.txt').on('input', function() {
+        if ($(this).val().length > 0) {
+            $('.btn').prop('disabled', false);
+        } else {
+            $('.btn').prop('disabled', true);
+        }
+    });
+
+    // 按钮点击事件
+    $('.btn').on('click', function() {
+        console.log('btn click');
+    });
+
+});
