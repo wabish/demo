86ce4c47c7de75458bb0430dea2248ce6489c986
cobish <cobish110@gmail.com>
2016-07-21 09:30:20 +0800

    further demo

--- react-vs-jquery-demo/jquery/index.js
+++ react-vs-jquery-demo/jquery/index.js
@@ -5,13 +5,16 @@ $(document).ready(function(){
 
     // 监听文本框输入，如果有值，则设置按钮可用
     $('.txt').on('input', function() {
-        if ($(this).val().length > 0) {
+        if ($(this).val().length > 0 && $(this).val().length <= 140) {
             $('.btn').prop('disabled', false);
         } else {
             $('.btn').prop('disabled', true);
         }
 
-        $('.num').text(140 - $(this).val().length);
+        // 剩余字数
+        var num = 140 - $(this).val().length;
+        num = (num > 0) ? num : 0;
+        $('.num').text(num);
     });
 
     // 按钮点击事件
