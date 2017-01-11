0e39421e145ac4e1870782f2543a1ed1e2330555
cobish <cobish110@gmail.com>
2017-01-11 13:44:21 +0800

    vue component

---
+++ vue-demo/vue-component/main.js
@@ -0,0 +1,19 @@
+// var Vue = require('vue');
+// var Hello = require('./components/hello');
+
+// new Vue({
+//   el: '#app',
+//   components: {
+//     'hello': Hello
+//   }
+// });
+
+import Vue from 'vue';
+import Hello from './components/hello';
+
+new Vue({
+  el: '#app',
+  components: {
+    'hello': Hello
+  }
+});
