8fb9978d3c4f9baf60efa622172a7e417d58f422
cobish <cobish110@gmail.com>
2017-02-20 18:55:40 +0800

    vue-framework

---
+++ vue-demo/vue-framework/src/components/button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :class="classes">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  const prefixCls = 'co-btn';
+
+  export default {
+    computed: {
+      classes() {
+        return `${prefixCls}-default`
+      }
+    }
+  };
+</script>
+
+<style scoped>
+.co-btn-default {
+  display: inline-block;
+  width: 250px;
+  height: 50px;
+  color: #fff;
+  background: 0;
+  border: 0;
+  background-color: #ffcccc;
+  font-size: 14px;
+  border-radius: 3px;
+  transition: all 0.2s ease;
+  cursor: pointer;
+}
+
+.co-btn-default:hover {
+  opacity: 0.7;
+}
+</style>
