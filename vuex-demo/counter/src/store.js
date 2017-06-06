5feed088aa3886ac04322b1a0dd5a22618c0b0a2
cobish <cobish110@gmail.com>
2017-06-06 15:08:49 +0800

    add vuex demo

---
+++ vuex-demo/counter/src/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+
+  mutations: {
+    increment(state) {
+      state.count += 1;
+    }
+  }
+});
+
+export default store;
